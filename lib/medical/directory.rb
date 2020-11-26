9d861bb72b7f57f9a8b78a112afc98f328e55dbe
Pranava S Balugari <stalin.pranava@gmail.com>
2020-11-26 21:32:39 +1100

    Add gateway file where all previous commit files are required

--- lib/medical/directory.rb
+++ lib/medical/directory.rb
@@ -1,8 +1,17 @@
 require "medical/directory/version"
+require "medical/directory/report"
+require "medical/directory/helpers"
+require "medical/directory/data_loader"
+require "medical/directory/health_record_history"
+require "medical/directory/instruction"
+require "medical/directory/cli"
 
 module Medical
   module Directory
     class Error < StandardError; end
-    # Your code goes here...
+    class FileNotExistsError < StandardError; end
+    class FileFormatNotSupportedError < StandardError; end
+
+    require "smarter_csv"
   end
 end
--- spec/medical/directory_spec.rb
+++ spec/medical/directory_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Medical::Directory do
   it "has a version number" do
     expect(Medical::Directory::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
